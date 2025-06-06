bc6d0859b9fe979ab1423deca14813e974849657
monisharema <monisharema97@gmail.com>
2025-06-06 13:52:33 +0300

    book put api created

--- index.js
+++ index.js
@@ -95,6 +95,21 @@ async function run() {
         console.log(result)
       })
 
+      // update books API
+      app.put('/book/:id',async (req,res) => {
+
+        const id = req.params.id;
+        console.log(id)
+        const filter = {_id: new ObjectId(id)};
+        const updatedBook = req.body;
+        const updatedBookData ={
+          $set:updatedBook
+        }
+        const result = await bookCollection.updateOne(filter,updatedBookData);
+        res.send(result)
+        console.log(result)
+        
+      })
 
 
 
